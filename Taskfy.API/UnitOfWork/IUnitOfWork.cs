14a0cab3af3e4efd70c4f90564b7dc3d0282e9f0
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:53:50 -0300

    feat: Implementa IUnitOfWork para unificar operações de repositórios
    - Centraliza operações de repositórios
    - CommitAsync encapsula a lógica de commit das transações.

---
+++ Taskfy.API/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Taskfy.API.Repositories.Usuarios;
+
+namespace Taskfy.API.UnitOfWork;
+
+public interface IUnitOfWork
+{
+	IUsuarioRepository UsuarioRepository { get; }
+	Task CommitAsync();
+}
