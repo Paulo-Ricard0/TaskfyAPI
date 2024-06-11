33d1ef9da28312efc95f194331a7438280ea5a24
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:24:45 -0300

    feat: Implementa modelo de Usuario com propriedades e relacionamentos
    - Herda propriedades e configurações da classe IdentityUser
    - Define RefreshToken e RefreshTokenExpiryTime para implementação de lógica de refreshToken
    - Estabelece relacionamento com Tarefa
    - Ignora serialização da propriedade Tarefas para evitar ser retornada na response

---
+++ Taskfy.API/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace Taskfy.API.Models;
+
+public class Usuario : IdentityUser
+{
+	public string? RefreshToken { get; set; }
+	public DateTime RefreshTokenExpiryTime { get; set; }
+
+	[JsonIgnore]
+	public ICollection<Tarefa>? Tarefas { get; set; }
+}
