3959f63aea979dd8a558857288415dac570a6d64
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:40:01 -0300

    feat: Implementa interface para serviços de autenticação
    - Define IAuthService com métodos para registro, login, gerenciamento de claims e tokens JWT.
    - Método RegisterAsync permite o registro de usuários através de um DTO.
    - LoginAsync permite o processo de login, retornando um objeto que pode conter informações do token de acesso.
    - GetAuthClaims extrai claims relevantes de um usuário para uso no token de acesso.
    - GenerateAccessToken gera um token de acesso JWT com base nas claims fornecidas e na configuração.
    - GenerateRefreshToken gera um token de refresh para manter a sessão do usuário.
    - ConvertUtcToBrasilTime converte datas UTC para o fuso horário do Brasil.

---
+++ Taskfy.API/Services/Auth/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Taskfy.API.DTOs;
+using Taskfy.API.DTOs.Usuario;
+using Taskfy.API.Models;
+
+namespace Taskfy.API.Services.Auth
+{
+	public interface IAuthService
+	{
+		Task<ResponseDTO> RegisterAsync(RegistroModelDTO usuarioModel);
+		Task<object> LoginAsync(LoginModelDTO usuarioModel);
+		Task<List<Claim>> GetAuthClaims(Usuario usuario);
+		JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);
+		string GenerateRefreshToken();
+		DateTime ConvertUtcToBrasilTime(DateTime utcDateTime);
+	}
+}
