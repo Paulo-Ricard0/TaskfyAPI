998e3552a61b2b9d78ab93f3283ff4cee6da5bd1
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:22:06 -0300

    feat: Implementa modelo de Tarefa com propriedades e relacionamentos
    - Adiciona classe Tarefa com propriedades Id, Titulo, Descricao, Data_vencimento, Status e Usuario_id
    - Define chave primária e geração automática de Id
    - Aplica validações de preenchimento e comprimento para Titulo e Descricao
    - Estabelece relacionamento com Usuario através de ForeignKey
    - Ignora serialização da propriedade Usuario para evitar ser retornado na response

---
+++ Taskfy.API/Models/Tarefa.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Taskfy.API.Models;
+
+public class Tarefa
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public Guid Id { get; set; }
+
+	[Required]
+	[StringLength(100)]
+	public string Titulo { get; set; } = string.Empty;
+
+	[Required]
+	[StringLength(255)]
+	public string Descricao { get; set; } = string.Empty;
+
+	[Required]
+	public DateTime Data_vencimento { get; set; }
+
+	public bool Status { get; set; } = false;
+
+	[Required]
+	public string Usuario_id { get; set; }
+
+	[ForeignKey("Usuario_id")]
+	[JsonIgnore]
+	public Usuario? Usuario { get; set; }
+}
