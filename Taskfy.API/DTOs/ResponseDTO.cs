373b178ae2fea982e2f86dbdde0c97a519e26185
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:11:29 -0300

    feat: Implementa padrão de response a ser retornado pelos metodos

---
+++ Taskfy.API/DTOs/ResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Taskfy.API.DTOs;
+
+public class ResponseDTO
+{
+	public string? Status { get; set; }
+	public string? Message { get; set; }
+
+	[JsonIgnore]
+	public int StatusCode { get; set; }
+}
