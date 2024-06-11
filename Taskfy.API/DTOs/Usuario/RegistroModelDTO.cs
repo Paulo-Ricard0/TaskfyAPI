f1fc3e17e142529b36f1ebbabf27e5ceb7c7131e
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:16:40 -0300

    feat: Adiciona DTO para modelo de registro de usuários
    - Implementa RegisterModelDTO com validações para campos Nome, Email e Senha
    - Utiliza DataAnnotations para validações de Nome, Email e Senha

---
+++ Taskfy.API/DTOs/Usuario/RegistroModelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskfy.API.DTOs.Usuario;
+
+public class RegistroModelDTO
+{
+	[Required(ErrorMessage = "Nome de usuário é obrigatório.")]
+	public string? UserName { get; set; }
+
+	[EmailAddress(ErrorMessage = "Email inválido.")]
+	[Required(ErrorMessage = "Email é obrigatório.")]
+	public string? Email { get; set; }
+
+	[Required(ErrorMessage = "Senha é obrigatória.")]
+	public string? Password { get; set; }
+}
