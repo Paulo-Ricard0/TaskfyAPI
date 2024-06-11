c93645688244eb2fd2a0e29539b9cdae5ecf1e7d
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 13:49:26 -0300

    refactor: Altera argumento do metodo FindByEmailAsync e renomeia variavel result
    - Altera argumento do metodoFindByEmailAsync
    - Renomeia variavel de result para resultado

--- Taskfy.Tests/Unit/Auth/Services/RegisterServiceTests.cs
+++ Taskfy.Tests/Unit/Auth/Services/RegisterServiceTests.cs
@@ -25,14 +25,20 @@ namespace Taskfy.Tests.Unit.Auth.Services
 			var userManager = Substitute.For<UserManager<Usuario>>(
 				Substitute.For<IUserStore<Usuario>>(), null, null, null, null, null, null, null, null);
 
-			userManager.FindByEmailAsync(Arg.Any<string>()).Returns(Task.FromResult<Usuario?>(null));
+			userManager.FindByEmailAsync(usuarioModel.Email).Returns(Task.FromResult<Usuario?>(null));
 			userManager.CreateAsync(Arg.Any<Usuario>(), Arg.Any<string>()).Returns(Task.FromResult(IdentityResult.Success));
 
 			var configuration = Substitute.For<IConfiguration>();
 			var authService = new AuthService(userManager, configuration);
 
 			// Act
-			var result = await authService.RegisterAsync(usuarioModel);
+			var resultado = await authService.RegisterAsync(usuarioModel);
+
+			// Assert
+			resultado.StatusCode.Should().Be(StatusCodes.Status201Created);
+			resultado.Status.Should().Be("Sucesso");
+			resultado.Message.Should().Be("Usuário criado com sucesso!");
+		}
 
 			// Assert
 			result.StatusCode.Should().Be(StatusCodes.Status201Created);
