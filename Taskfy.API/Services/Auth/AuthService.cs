3ba00c8a68c5edb81b594399a4e5f04a14c41ad6
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:44:25 -0300

    feat: Implementa lógica para registro e login de usuários
    - O método RegisterAsync verifica se o usuário já existe antes de tentar criar um novo, retornando um erro caso já esteja registrado.
    - Utiliza UserManager para interagir com o banco de dados, criando um novo usuário com email, nome de usuário e senha
    - O método LoginAsync busca o usuário pelo email e verifica a senha, retornando um objeto contendo token de acesso e refresh token se as credenciais forem válidas.
    - Gera claims de autenticação com base no usuário logado e cria tokens JWT para acesso e refresh

---
+++ Taskfy.API/Services/Auth/AuthService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using Taskfy.API.DTOs;
+using Taskfy.API.DTOs.Usuario;
+using Taskfy.API.Models;
+
+namespace Taskfy.API.Services.Auth;
+
+public class AuthService : IAuthService
+{
+	private readonly UserManager<Usuario> _userManager;
+	private readonly IConfiguration _configuration;
+
+	public AuthService(UserManager<Usuario> userManager, IConfiguration configuration)
+	{
+		_userManager = userManager;
+		_configuration = configuration;
+	}
+
+	public async Task<ResponseDTO> RegisterAsync(RegistroModelDTO usuarioModel)
+	{
+		var usuarioExiste = await _userManager.FindByEmailAsync(usuarioModel.Email!);
+		if (usuarioExiste != null)
+		{
+			return new ResponseDTO { Status = "Erro", Message = "Usuário já registrado!", StatusCode = StatusCodes.Status409Conflict };
+		}
+
+		Usuario usuario = new()
+		{
+			Email = usuarioModel.Email,
+			UserName = usuarioModel.UserName,
+			SecurityStamp = Guid.NewGuid().ToString()
+		};
+
+		var registraUsuario = await _userManager.CreateAsync(usuario, usuarioModel.Password!);
+
+		if (registraUsuario.Succeeded)
+		{
+			return new ResponseDTO
+			{
+				Status = "Sucesso",
+				Message = "Usuário criado com sucesso!",
+				StatusCode = StatusCodes.Status201Created
+			};
+		}
+
+		return new ResponseDTO
+		{
+			Status = "Erro",
+			Message = "Falha na criação de usuário.",
+			StatusCode = StatusCodes.Status500InternalServerError
+		};
+	}
+
+	public async Task<object> LoginAsync(LoginModelDTO usuarioModel)
+	{
+		var usuario = await _userManager.FindByEmailAsync(usuarioModel.Email!);
+		if (usuario == null || !await _userManager.CheckPasswordAsync(usuario, usuarioModel.Password!))
+			return null;
+
+		var authClaims = await GetAuthClaims(usuario);
+		var token = GenerateAccessToken(authClaims, _configuration);
+		var refreshToken = GenerateRefreshToken();
+
+		if (!int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes))
+			throw new InvalidOperationException("Período de validade do refresh token inválido.");
+
+		usuario.RefreshToken = refreshToken;
+		usuario.RefreshTokenExpiryTime = ConvertUtcToBrasilTime(DateTime.UtcNow).AddMinutes(refreshTokenValidityInMinutes);
+
+		var atualizaUsuario = await _userManager.UpdateAsync(usuario);
+
+		if (!atualizaUsuario.Succeeded)
+			throw new InvalidOperationException("Falha ao atualizar refresh token do usuário.");
+
+		return new
+		{
+			Token = new JwtSecurityTokenHandler().WriteToken(token),
+			RefreshToken = refreshToken,
+			Expiration = token.ValidTo
+		};
+	}
