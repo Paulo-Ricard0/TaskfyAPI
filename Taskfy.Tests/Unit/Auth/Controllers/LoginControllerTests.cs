2b87c338528de91e1f1f7d9a21df341494af6108
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 19:16:04 -0300

    tests: Adiciona teste para Login com sucesso
    - Verifica se o controller está chamando corretamente o authService e retornando o token e um status code 200 para um login com sucesso

---
+++ Taskfy.Tests/Unit/Auth/Controllers/LoginControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Taskfy.API.Controllers;
+using Taskfy.API.DTOs;
+using Taskfy.API.DTOs.Usuario;
+using Taskfy.API.Services.Auth;
+
+namespace Taskfy.Tests.Unit.Auth.Controllers;
+
+public class LoginControllerTests
+{
+	[Fact]
+	public async Task Verifica_LoginComSucesso_Retorna201CreatedStatusCode()
+	{
+		// Arrange
+		var usuarioModel = new LoginModelDTO
+		{
+			Email = "test@gmail.com",
+			Password = "Test123@"
+		};
+
+		var responseToken = new ResponseLoginTokenDTO
+		{
+			StatusCode = StatusCodes.Status200OK,
+			Token = "mocked.jwt.token",
+			RefreshToken = "mocked.refresh.token",
+			Expiration = DateTime.UtcNow.AddMinutes(30)
+		};
+
+		var authService = Substitute.For<IAuthService>();
+		authService.LoginAsync(usuarioModel).Returns(Task.FromResult(responseToken as ResponseDTO));
+
+		var controller = new AuthController(authService);
+
+		// Act
+		var resultado = await controller.Login(usuarioModel) as ObjectResult;
+
+		// Assert
+		resultado.Should().NotBeNull();
+		resultado.StatusCode.Should().Be(StatusCodes.Status200OK);
+		resultado.Value.Should().BeEquivalentTo(responseToken);
+	}
+}
