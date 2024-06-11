c98bc140f81fec6fbc8df6f6b9949ed19b502552
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:14:53 -0300

    feat: Adiciona DTO para modelo de login de usuários
    - Implementa LoginModelDTO com validações para campos Email e Senha
    - Utiliza DataAnnotations para exigir preenchimento de Email e Senha

---
+++ Taskfy.API/DTOs/Usuario/LoginModelDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskfy.API.DTOs.Usuario;
+
+public class LoginModelDTO
+{
+	[Required(ErrorMessage = "Email é obrigatório.")]
+	public string? Email { get; set; }
+
+	[Required(ErrorMessage = "Senha é obrigatória.")]
+	public string? Password { get; set; }
+}
