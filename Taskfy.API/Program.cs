aff2501b14405e4d9460cf62f3fbc1fe3424a011
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 09:51:17 -0300

    chore: Configura services, identity e authorization
    Configura injeções de dependencia, autorizações, autenticação e build

--- Taskfy.API/Program.cs
+++ Taskfy.API/Program.cs
@@ -1,14 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Taskfy.API.Data;
+using Taskfy.API.Models;
+using Taskfy.API.Repositories;
+using Taskfy.API.Repositories.Usuarios;
+using Taskfy.API.Services.Auth;
+using Taskfy.API.UnitOfWork;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adiciona serviços ao container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Configura Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>()
+	.AddEntityFrameworkStores<AppDbContext>()
+	.AddDefaultTokenProviders();
+
+// Configura a conexão do banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura requisições http
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
