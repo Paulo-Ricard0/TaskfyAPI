7600269506e79f73d932aebc7bc01b27c1f0de0b
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 11:01:36 -0300

    feat: Adiciona teste unitário para verificação de criação de usuário
    - Verifica se o controller está chamando corretamente o authService e retornando um status code 201
    - Utiliza NSubstitute para mockar o serviço de autenticação e definir seu comportamento esperado.

---
+++ Taskfy.Tests/Unit/Auth/Controllers/RegisterControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Taskfy.API.Controllers;
+using Taskfy.API.DTOs;
+using Taskfy.API.DTOs.Usuario;
+using Taskfy.API.Services.Auth;
+
+namespace Taskfy.Tests.Unit.Auth.Controllers
+{
+	public class RegisterControllerTests
+	{
+		[Fact]
+		public async Task Verifica_UsuarioCriado_Retorna201CreatedStatusCode()
+		{
+			// Arrange
+			var usuarioModel = new RegistroModelDTO
+			{
+				UserName = "testuser",
+				Email = "test@gmail.com",
+				Password = "Test123@"
+			};
+
+			var responseDto = new ResponseDTO
+			{
+				Status = "Sucesso",
+				Message = "Usuário criado com sucesso!",
+				StatusCode = StatusCodes.Status201Created
+			};
+
+			var authService = Substitute.For<IAuthService>();
+			authService.RegisterAsync(usuarioModel).Returns(Task.FromResult(responseDto));
+
+			var controller = new AuthController(authService);
+
+			// Act
+			var result = await controller.Register(usuarioModel) as ObjectResult;
+
+			// Assert
+			result.Should().NotBeNull();
+			result.StatusCode.Should().Be(StatusCodes.Status201Created);
+			result.Value.Should().BeEquivalentTo(responseDto);
+		}
+	}
+}
