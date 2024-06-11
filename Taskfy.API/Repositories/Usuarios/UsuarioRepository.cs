5c7fcdaa0f4cedc916696a3dd1764e268723702d
Paulo Ricardo M <pr741234@gmail.com>
2024-06-11 10:35:20 -0300

    feat: Adiciona UsuarioRepository para operações de usuario
    - Herda operações de CRUD de Usuario da classe generica Repository
    - Implementa IUsuarioRepository para operações personalizadas

---
+++ Taskfy.API/Repositories/Usuarios/UsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using Taskfy.API.Data;
+using Taskfy.API.Models;
+
+namespace Taskfy.API.Repositories.Usuarios;
+
+public class UsuarioRepository : Repository<Usuario>, IUsuarioRepository
+{
+	public UsuarioRepository(AppDbContext context) : base(context)
+	{
+	}
+}
