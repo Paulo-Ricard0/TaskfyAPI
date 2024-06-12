ae9c51516661766b21321982bcb63f985f7424d9
Paulo Ricardo M <pr741234@gmail.com>
2024-06-12 14:53:28 -0300

    feat: Adiciona extensão para middleware de exceções
    - Permite uma integração mais simples do GlobalExceptionHandlerMiddleware nas requisições

---
+++ Taskfy.API/Middlewares/GlobalExceptionHandlerExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskfy.API.Middlewares;
+
+public static class GlobalExceptionHandlerExtensions
+{
+	public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
+	{
+		return builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+	}
+}
