1f5b396ca3b69e42223e74e6e5697d5a58abb599
Paulo Ricardo M <pr741234@gmail.com>
2024-06-12 14:51:01 -0300

    feat: Add middleware para tratamento de exceções
    - Adiciona o middleware GlobalExceptionHandlerMiddleware para capturar e tratar exceções lançadas durante o processamento das requisições HTTP.
    - Adiciona Log para registrar detalhes dos erros

---
+++ Taskfy.API/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Text.Json;
+using Taskfy.API.Logs;
+
+namespace Taskfy.API.Middlewares;
+
+public class GlobalExceptionHandlerMiddleware
+{
+	private readonly RequestDelegate _next;
+
+	public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+	{
+		_next = next;
+	}
+
+	public async Task InvokeAsync(HttpContext context)
+	{
+		try
+		{
+			await _next(context);
+		}
+		catch (Exception ex)
+		{
+			var logger = context.RequestServices.GetRequiredService<ILog>();
+			await HandleExceptionAsync(context, ex, logger);
+		}
+	}
+
+	private static Task HandleExceptionAsync(HttpContext context, Exception exception, ILog logger)
+	{
+		var statusCode = HttpStatusCode.InternalServerError;
+		var response = new
+		{
+			status = "Erro",
+			message = "Ocorreu um erro interno no servidor.",
+			error = exception.Message,
+		};
+
+		var log = new
+		{
+			message = "Ocorreu um erro interno no servidor.",
+			error = exception.Message,
+			stackTrace = exception.StackTrace,
+			exception = exception.InnerException,
+			source = exception.Source,
+		};
+
+		var responseContent = JsonSerializer.Serialize(response);
+		var logContent = JsonSerializer.Serialize(log);
+
+		logger.LogErrorToFile("Erro interno do servidor.", logContent);
+
+		context.Response.ContentType = "application/json";
+		context.Response.StatusCode = (int)statusCode;
+
+		return context.Response.WriteAsync(responseContent);
+	}
+}
