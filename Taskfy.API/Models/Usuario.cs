0d739c5c27e08a59bdc4a0cf23204c01d31b590d
Paulo Ricardo M <pr741234@gmail.com>
2024-06-12 14:46:06 -0300

    feat: Adiciona propriedade Name ao modelo de usuario

--- Taskfy.API/Models/Usuario.cs
+++ Taskfy.API/Models/Usuario.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Taskfy.API.Models;
 
 public class Usuario : IdentityUser
 {
+	[Required]
+	[StringLength(100)]
+	public string Name { get; set; } = string.Empty;
 	public string? RefreshToken { get; set; }
 	public DateTime RefreshTokenExpiryTime { get; set; }
 
