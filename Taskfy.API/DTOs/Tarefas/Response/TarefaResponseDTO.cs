f239bde89630c6ff65cad87093972cf4a6e93134
Paulo Ricardo M <pr741234@gmail.com>
2024-06-13 14:33:31 -0300

    feat: Adiciona DTO de response para retorno de Tarefas

---
+++ Taskfy.API/DTOs/Tarefas/Response/TarefaResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Taskfy.API.DTOs.Tarefas.Response;
+
+public class TarefaResponseDTO : ResponseDTO
+{
+	public TarefaDTO? Data { get; set; }
+
+	[JsonIgnore]
+	public override string Status { get; set; } = string.Empty;
+	[JsonIgnore]
+	public override string Message { get; set; } = string.Empty;
+}
