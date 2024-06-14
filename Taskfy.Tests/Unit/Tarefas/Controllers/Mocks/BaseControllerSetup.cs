860c4093a4adffc3685edba58c0154af8a83abc7
Paulo Ricardo M <pr741234@gmail.com>
2024-06-14 11:15:31 -0300

    test: Cria classe base para mock de dependencias externas

---
+++ Taskfy.Tests/Unit/Tarefas/Controllers/Mocks/BaseControllerSetup.cs
@@ -0,0 +1,14 @@
+﻿using NSubstitute;
+using Taskfy.API.Services.Tarefas;
+
+namespace Taskfy.Tests.Unit.Tarefas.Controllers.Mocks;
+
+public abstract class BaseControllerSetup
+{
+	protected readonly ITarefaService TarefaServiceMock;
+
+	protected BaseControllerSetup()
+	{
+		TarefaServiceMock = Substitute.For<ITarefaService>();
+	}
+}
