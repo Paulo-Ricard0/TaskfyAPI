4f061296c975d4e0228c5e264bcc742d3b2e3b38
Paulo Ricardo M <pr741234@gmail.com>
2024-06-16 23:12:08 -0300

    feat: Adiciona DTO de request para atualização de tarefa

---
+++ Taskfy.API/DTOs/Tarefas/Request/TarefaRequestUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskfy.API.DTOs.Tarefas.Request;
+
+public class TarefaRequestUpdateDTO : TarefaRequestDTO
+{
+	[Required(ErrorMessage = "O Status é obrigatório")]
+	public bool Status { get; set; } = false;
+}
