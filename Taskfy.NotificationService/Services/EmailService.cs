3ca1399388699f99b14b431fd0496561e1b0ff37
Paulo Ricardo M <pr741234@gmail.com>
2024-07-23 12:41:04 -0300

    feat: Cria template para o subject do email

---
+++ Taskfy.NotificationService/Services/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using Taskfy.NotificationService.Models;
+using Taskfy.NotificationService.Services.Interfaces;
+
+namespace Taskfy.NotificationService.Services;
+public class EmailService : IEmailService
+{
+	public string GetEmailSubject(NotificationType notificationType, string? tituloTarefa)
+	{
+		return notificationType switch
+		{
+			NotificationType.UserCreated => "Bem-vindo ao Taskfy!",
+			NotificationType.TaskCreated => $"Nova tarefa criada: {tituloTarefa}",
+			NotificationType.TaskUpdated => $"Tarefa atualizada: {tituloTarefa}",
+			NotificationType.TaskDeleted => $"Tarefa deletada: {tituloTarefa}",
+			_ => "Notificação Taskfy"
+		};
+	}
+}
---
+++ Taskfy.NotificationService/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿using Taskfy.NotificationService.Models;
+
+namespace Taskfy.NotificationService.Services.Interfaces;
+public interface IEmailService
+{
+	string GetEmailSubject(NotificationType notificationType, string? tituloTarefa);
+}
