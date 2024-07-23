377b558b7d3a56073d6cc0368d9c0e99c8547011
Paulo Ricardo M <pr741234@gmail.com>
2024-07-23 12:59:13 -0300

    feat: Cria models de email e de SmtpSettings

---
+++ Taskfy.EmailSender/Models/EmailDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskfy.EmailSender.Models;
+public class EmailDetails
+{
+	public required string UserName { get; set; }
+	public required string ToEmail { get; set; }
+	public EmailType Type { get; set; }
+	public string? TituloTarefa { get; set; }
+	public string? DescricaoTarefa { get; set; }
+	public string? DataVencimentoTarefa { get; set; }
+}
---
+++ Taskfy.EmailSender/Models/SmtpSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskfy.EmailSender.Models;
+public class SmtpSettings
+{
+	public required string Host { get; set; }
+	public required int Port { get; set; }
+	public required string Name { get; set; }
+	public required string Password { get; set; }
+	public required string FromEmail { get; set; }
+	public required bool EnableSsl { get; set; }
+}
