27582d6bd05fe6bbe7bd6f36eb1cbe0197b7fcb4
Paulo Ricardo M <pr741234@gmail.com>
2024-07-23 12:17:35 -0300

    feat: Integração de mensageria com RabbitMQ
    - Adiciona integração com fila de mensagens para notificação de criação de usuários

---
+++ Taskfy.API/Services/MessagesQueue/IMessageQueueService.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskfy.API.Services.MessagesQueue;
+
+public interface IMessageQueueService
+{
+	void PublishUserCreatedMessage(string userName, string email);
+	void PublishTaskCreatedMessage(string userName, string email, string tituloTarefa, string descricaoTarefa, string dataVencimentoTarefa);
+	void PublishTaskUpdatedMessage(string userName, string email, string tituloTarefa, string descricaoTarefa, string dataVencimentoTarefa);
+	void PublishTaskDeletedMessage(string userName, string email, string tituloTarefa);
+}
---
+++ Taskfy.API/Services/MessagesQueue/MessageQueueService.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Taskfy.API.Services.MessagesQueue;
+
+public enum NotificationType
+{
+	UserCreated,
+	TaskCreated,
+	TaskUpdated,
+	TaskDeleted
+}
+
+public class MessageQueueService : IMessageQueueService
+{
+	private readonly IConnection _connection;
+	private readonly IModel _channel;
+
+	public MessageQueueService(IConnection connection, IModel channel)
+	{
+		_connection = connection;
+		_channel = channel;
+	}
+
+	public void PublishUserCreatedMessage(string userName, string email)
+	{
+		var message = new
+		{
+			UserName = userName,
+			ToEmail = email,
+			Type = NotificationType.UserCreated
+		};
+
+		PublishMessage("taskfy_notification_queue", message);
+	}
+
+	public void PublishTaskCreatedMessage(string userName, string email, string tituloTarefa, string descricaoTarefa, string dataVencimentoTarefa)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void PublishTaskDeletedMessage(string userName, string email, string tituloTarefa)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void PublishTaskUpdatedMessage(string userName, string email, string tituloTarefa, string descricaoTarefa, string dataVencimentoTarefa)
+	{
+		throw new NotImplementedException();
+	}
+
+	private void PublishMessage(string queueName, object message)
+	{
+		var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+		_channel.BasicPublish(exchange: "",
+							routingKey: queueName,
+							basicProperties: null,
+							body: body);
+	}
+
+	public void Dispose()
+	{
+		_channel.Close();
+		_connection.Close();
+	}
+}
