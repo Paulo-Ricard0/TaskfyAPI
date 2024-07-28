dd74ab277bf1faecdd977e74ddf27decff9c30df
Paulo Ricardo M <pr741234@gmail.com>
2024-07-28 11:00:27 -0300

    tests: Configura setup base de injeção de dependências e mocks

---
+++ Taskfy.Tests/Unit/Auth/Controllers/BaseMocks/BaseAuthControllerSetup.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+using Taskfy.API.Controllers;
+using Taskfy.API.Services.Auth;
+
+namespace Taskfy.Tests.Unit.Auth.Controllers.BaseMocks;
+public abstract class BaseAuthControllerSetup
+{
+	protected readonly IAuthService AuthServiceMock;
+	protected readonly AuthController AuthControllerMock;
+
+	protected BaseAuthControllerSetup()
+	{
+		AuthServiceMock = Substitute.For<IAuthService>();
+		AuthControllerMock = new AuthController(AuthServiceMock);
+	}
+}
