9fb6b20ada96a4644fd0bf312f32c4b121f6ca72
Paulo Ricardo M <pr741234@gmail.com>
2024-07-28 11:02:26 -0300

    tests: Configura setup base para injeção de dependências e mocks

---
+++ Taskfy.Tests/Unit/Auth/Services/BaseMocks/BaseUserServiceSetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using Taskfy.API.Logs;
+using Taskfy.API.Models;
+using Taskfy.API.Services.Auth;
+using Taskfy.API.Services.MessagesQueue;
+
+namespace Taskfy.Tests.Unit.Auth.Services.Mocks;
+public abstract class BaseUserServiceSetup
+{
+	protected readonly UserManager<Usuario> UserManagerMock;
+	protected readonly IConfiguration ConfigurationMock;
+	protected readonly ITokenService TokenServiceMock;
+	protected readonly ILog LoggerMock;
+	protected readonly IMessageQueueService MessageQueueServiceMock;
+	protected readonly IAuthService AuthServiceMock;
+
+	protected BaseUserServiceSetup()
+	{
+		UserManagerMock = Substitute.For<UserManager<Usuario>>(
+				Substitute.For<IUserStore<Usuario>>(), null, null, null, null, null, null, null, null);
+
+		ConfigurationMock = Substitute.For<IConfiguration>();
+		TokenServiceMock = Substitute.For<ITokenService>();
+		LoggerMock = Substitute.For<ILog>();
+		MessageQueueServiceMock = Substitute.For<IMessageQueueService>();
+
+		AuthServiceMock = new AuthService(UserManagerMock, ConfigurationMock, TokenServiceMock, LoggerMock, MessageQueueServiceMock);
+	}
+}
